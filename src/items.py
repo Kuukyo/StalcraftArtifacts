4b5131b22e4f2e217a403b2b711e922d2a88f61c
Kuukyo <kvvkyo@gmail.com>
2023-05-28 19:21:08 +0200

    cleaned up code

--- src/items.py
+++ src/items.py
@@ -34,41 +34,26 @@ class Artifact:
         artifacts = lib.load_mem("../resources/artifacts.json")
         artifact = artifacts[name]
 
+        properties = artifacts[name].keys()
+        spcl_properties = ["psy_emissions", "temperature", "radiation", "biological_infection", "frost"]
+
         self.name = name
         self.quality = quality
         self.potential = potential
-        self.vitality = get_value(artifact["vitality"], quality, potential)
-        self.stamina_regen = get_value(artifact["stamina_regen"], quality, potential)
-        self.psy_emissions = get_value(artifact["psy_emissions"], quality, potential, True)
-        self.psy_emission_protection = get_value(artifact["psy_emissions_protection"], quality, potential)
-        self.stamina = get_value(artifact["stamina"], quality, potential)
-        self.movement_speed = get_value(artifact["movement_speed"], quality, potential)
-        self.temperature = get_value(artifact["temperature"], quality, potential, True)
-        self.reaction_to_electricity = get_value(artifact["reaction_to_electricity"], quality, potential)
-        self.radiation = get_value(artifact["radiation"], quality, potential, True)
-        self.biological_infection = get_value(artifact["biological_infection"], quality, potential, True)
-        self.thermal_protection = get_value(artifact["thermal_protection"], quality, potential)
-        self.psy_emission_resistance = get_value(artifact["psy_emission_resistance"], quality, potential)
-        self.carry_weight = get_value(artifact["carry_weight"], quality, potential)
-        self.radiation_protection = get_value(artifact["radiation_protection"], quality, potential)
-        self.healing_effectiveness = get_value(artifact["healing_effectiveness"], quality, potential)
-        self.bullet_resistance = get_value(artifact["bullet_resistance"], quality, potential)
-        self.explosion_protection = get_value(artifact["explosion_protection"], quality, potential)
-        self.radiation_resistance = get_value(artifact["radiation_resistance"], quality, potential)
-        self.bioinfection_resistance = get_value(artifact["bioinfection_resistance"], quality, potential)
-        self.thermal_resistance = get_value(artifact["thermal_resistance"], quality, potential)
-        self.health_regeneration = get_value(artifact["health_regeneration"], quality, potential)
-        self.bleeding = get_value(artifact["bleeding"], quality, potential)
-        self.bleeding_protection = get_value(artifact["bleeding_protection"], quality, potential)
-        self.reaction_to_burns = get_value(artifact["reaction_to_burns"], quality, potential)
-        self.frost = get_value(artifact["frost"], quality, potential, True)
-        self.melee_protection = get_value(artifact["melee_protection"], quality, potential)
-        self.reaction_to_melee = get_value(artifact["reaction_to_melee"], quality, potential)
-        self.resistance_to_fire = get_value(artifact["resistance_to_fire"], quality, potential)
-        self.resistance_to_chemicals = get_value(artifact["resistance_to_chemicals"], quality, potential)
-        self.resistance_to_chemical_burns = get_value(artifact["resistance_to_chemical_burns"], quality, potential)
-        self.bioinfection_protection = get_value(artifact["bioinfection_protection"], quality, potential)
         self.extra_properties = artifact["extra_properties"]
+
+        for prop in properties:
+            if prop == "extra_properties":
+                continue
+
+            value = artifact[prop]
+            is_spcl = False
+
+            if prop in spcl_properties:
+                is_spcl = True
+
+            value_ = get_value(value, quality, potential, is_spcl)
+            setattr(self, prop, value_)
 
     def add_property(self, property, prop_range):
         attr = getattr(self, property)
@@ -88,7 +73,7 @@ class Artifact:
             res += f"Stamina Regen: {self.stamina_regen}%\n"
         if self.psy_emissions != 0:
             res += f"Psy-emissions: {self.psy_emissions}\n"
-        if self.psy_emission_protection != 0:
+        if self.psy_emissions_protection != 0:
             res += f"Psy-Emission Protection: {self.psy_emission_protection}\n"
         if self.stamina != 0:
             res += f"Stamina: {self.stamina}%\n"
